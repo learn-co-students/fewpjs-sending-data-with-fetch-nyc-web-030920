22815d84427d9578f3550d2d8e5f4fb921afc187
CBreakr <github email address>
2020-04-25 17:32:35 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Add your code here
+const headers = {
+    "content-type": "application/json",
+    "accept": "application/json"
+};
+
+function submitData(name, email){
+    const input = {name, email};
+
+    return fetch("http://localhost:3000/users", {
+        method:"POST",
+        headers,
+        body: JSON.stringify(input)
+    })
+    .then(res => res.json())
+    .then(data => {
+        // console.log("DATA", data, "DATA");
+        // const idNode = document.createElement("div");
+        // idNode.textContent = data.id;
+        // document.body.append(idNode);
+        document.body.innerHTML = data.id;
+    })
+    .catch(err => {
+        // console.log("error", err);
+        const errNode = document.createElement("div");
+        errNode.textContent = err.message;
+        document.body.append(errNode);
+    });
+}
