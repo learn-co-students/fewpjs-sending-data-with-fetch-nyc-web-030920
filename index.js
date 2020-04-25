d18f4722d66b25e2b6dfdee168a5f675e41ad3a1
Vivica <Ma>
2020-04-25 17:18:26 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// Add your code here
+const url = "http://localhost:3000/users"
+const requestHeader = {"Content-Type": "application/json",
+                        "Accept":"application/json"}
+
+function submitData(name, email){
+    return fetch(url,{
+        method: "POST",
+        headers: requestHeader,
+        body: JSON.stringify({
+            name, 
+            email
+        })
+    })
+    .then(resp => resp.json())
+    .then(function (user){
+        document.body.innerHTML = user["id"]
+    })   
+    .catch(function(error){
+        document.body.innerHTML = error.message
+    })
+}
+    
+    
+
